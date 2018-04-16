b84bd602652e0c477fb6213f731552d52e7aabbf
Daniel Wong <drojfjord@gmail.com>
2018-04-16 20:11:26 +1000

    Initial Commit

---
+++ umineko_cs_installer/umineko_question.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace umineko_cs_installer
+{
+    class umineko_question
+    {
+        string basePath;
+
+        string relPath(string path)
+        {
+            return Path.Combine(basePath, path)
+        }
+
+        void doInstall()
+        {
+            basePath = @"c:\temp4\test";
+
+            //check file exists
+            File.Exists(relPath("arc.nsa"));
+
+        }
+    }
+}
